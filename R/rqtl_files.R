fc4a34488b365aaeebbd109c04b100653a862214
Tom Ellis <thomas.ellis@ebc.uu.se>
2018-08-01 14:56:21 +0200

    Updated rqtl_files.R and its output to ensure this matches Chris' scripts.

--- R/rqtl_files.R
+++ R/rqtl_files.R
@@ -25,23 +25,30 @@ ffit <- read.csv('data_files/Agren2013_LSMfitness.csv')
 ffit <- ffit[,c("id","F10_NFruitIt_noedge_lsm", "F11_NFruitIt_noedge_lsm", "F10_NFruitSw_noedge_lsm", "F11_NFruitSw_noedge_lsm")]
 
 # rename columns
-colnms <- c("genotype", "mean_it2010", "mean_it2011", "mean_sw2010", "mean_sw2011")
+colnms <- c("id", "it2010", "it2011", "sw2010", "sw2011")
 colnames(seed) <- colnames(mass) <- colnames(frut) <- colnames(ffit) <- colnames(surv) <- colnms
 
+# Files for R/QTL for traits from Ågrne etal 2013.
+# This will throw a warning about NA labels.
+# This is because not all lines are included across datasets.
+write.csv(qtl_dataframe(surv, geno), file = "data_files/rqtl_surv.csv", row.names = F)
+write.csv(qtl_dataframe(frut, geno), file = "data_files/rqtl_frut.csv", row.names = F)
+write.csv(qtl_dataframe(ffit, geno), file = "data_files/rqtl_ffit.csv", row.names = F)
+
 # reorder rows to a common order.
-rilnames <- sort(unique(c(frut$genotype, seed$genotype, ffit$genotype)))
-mass <- mass[match(rilnames, mass$genotype),]
-seed <- seed[match(rilnames, seed$genotype),]
-frut <- frut[match(rilnames, frut$genotype),]
-ffit <- ffit[match(rilnames, ffit$genotype),]
-surv <- surv[match(rilnames, surv$genotype),]
+rilnames <- sort(unique(c(frut$id, seed$id, ffit$id)))
+mass <- mass[match(rilnames, mass$id),]
+seed <- seed[match(rilnames, seed$id),]
+frut <- frut[match(rilnames, frut$id),]
+ffit <- ffit[match(rilnames, ffit$id),]
+surv <- surv[match(rilnames, surv$id),]
 
 # seeds per adult
 tofu <- seed[,2:5] * frut[,2:5]
-tofu <- cbind(genotype = frut$genotype, tofu)
+tofu <- cbind(genotype = frut$id, tofu)
 # seeds per seedling
 tfit <- seed[,2:5] * ffit[,2:5]
-tfit <- cbind(genotype = ffit$genotype, tfit)
+tfit <- cbind(genotype = ffit$id, tfit)
 
 # write phenotypes to disk
 write.csv(surv, file = "data_files/RIL_survival.csv", row.names = F)
@@ -52,12 +59,10 @@ write.csv(tofu, file = "data_files/RIL_seeds_per_adult.csv", row.names = F)
 write.csv(ffit, file = "data_files/RIL_fruits_per_seedling.csv", row.names = F)
 write.csv(tfit, file = "data_files/RIL_seeds_per_seedling.csv", row.names = F)
 
-# Files for R/QTL. This will throw a warning about NA labels.
+# Files for R/QTL for new traits, and total seed number traits.
+# This will throw a warning about NA labels.
 # This is because not all lines are included across datasets.
-write.csv(qtl_dataframe(surv, geno), file = "data_files/rqtl_surv.csv", row.names = F)
 write.csv(qtl_dataframe(mass, geno), file = "data_files/rqtl_mass.csv", row.names = F)
 write.csv(qtl_dataframe(seed, geno), file = "data_files/rqtl_seed.csv", row.names = F)
-write.csv(qtl_dataframe(frut, geno), file = "data_files/rqtl_frut.csv", row.names = F)
 write.csv(qtl_dataframe(tofu, geno), file = "data_files/rqtl_tofu.csv", row.names = F)
-write.csv(qtl_dataframe(ffit, geno), file = "data_files/rqtl_ffit.csv", row.names = F)
-write.csv(qtl_dataframe(tfit, geno), file = "data_files/rqtl_tfit.csv", row.names = F)
+
