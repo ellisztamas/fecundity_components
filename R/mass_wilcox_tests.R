a251c693257c9d466442a5a8653c65e936629c1e
Tom Ellis <thomas.ellis@ebc.uu.se>
2018-08-01 14:44:12 +0200

    Added script to perform wilcox tests for seed mass.

---
+++ R/mass_wilcox_tests.R
@@ -0,0 +1,13 @@
+parent_mass <- read.csv('data_files/individual_parents_massnumber.csv')
+# unique identifier for each fite year combination
+parent_mass$siteyear <- paste(parent_mass$site, parent_mass$year,sep="_")
+siteyear <- sort(unique(parent_mass$siteyear))
+
+# empty lists to store tests
+mass_wilcox <- vector('list', length(siteyear))
+names(mass_wilcox) <- siteyear
+# run tests
+for(y in siteyear){ # seed number
+  this_exp <- seed_par[parent_mass$siteyear == y,]
+  mass_wilcox[[y]] <- wilcox.test(mean_mass_ug ~ genotype, data=this_exp)
+}
