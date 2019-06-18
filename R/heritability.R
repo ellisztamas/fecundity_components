c3db7ebcaa003b1de55f10d62c5c818d17f3d92d
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2019-06-18 15:48:06 +0200

    Added script R/heritability.R to calculate broad-sense heritabilities and assess uncertainty via parametric bootstrap. This saves to an RDS object in /output. Added section to the methods and results outlining this.
    Also attempted to make progress with the discussion section on adaptive differentiation, but made no meaningful progress. Need to think carefully about what to say.

---
+++ R/heritability.R
@@ -0,0 +1,61 @@
+library(lme4)
+
+# Import and subset data
+ind_data <- read.csv("data_raw/RILFieldExptItalySweden_SeedSizeNumber_SixYrs_398RIL Excl Edge.csv")
+ind_data <- ind_data[ind_data$Year %in% 2010:2011,]
+ind_data$Tray <- as.factor(ind_data$Tray)
+# Add a column for survival
+ind_data$survival <- ifelse(ind_data$NFruits > 0, 1, 0)
+# Vector of traits of interest
+traits <- c("survival", "Fecundity","X..Seeds..good.", "NFruits", "TSdPerFrPrPlant_IndEst", "MSeedMass.ug.")
+# Vector of site_year identifiers.
+ind_data$site_year <- paste(ind_data$Site, ind_data$Year)
+site_year <- unique(ind_data$site_year)
+
+# Function to get broad-sense heritability from a mixed model.
+herit <- function(model){
+  vc <- as.data.frame(VarCorr(model))$vcov
+  vc[1] / sum(vc)
+}
+
+# Observed heritabilities
+# Empty df to store values
+H2 <- list(
+  obs = data.frame(matrix(NA, nrow = length(traits), ncol = length(site_year))),
+  upper = data.frame(matrix(NA, nrow = length(traits), ncol = length(site_year))),
+  lower = data.frame(matrix(NA, nrow = length(traits), ncol = length(site_year)))
+)
+# Label columns and rows
+colnames(H2$obs)  <- colnames(H2$upper)  <- colnames(H2$lower)  <- site_year
+row.names(H2$obs) <- row.names(H2$upper) <- row.names(H2$lower) <- traits
+
+nreps <- 200
+pb = txtProgressBar(min = 0, max = nreps, initial = 0)#set up progress bar
+t0 <- proc.time()
+for(t in traits){
+  # Update progress bar
+  setTxtProgressBar(pb, t)
+  
+  model <- as.formula(paste(t, "~ Tray + (1| Genotype2)"))
+  for(sy in site_year){
+    # Fit the model
+    m <- lmer(model,
+              data   = ind_data,
+              subset = site_year == sy)
+    # Pull out variance components and get H2.
+    vc <- as.data.frame(VarCorr(m))$vcov
+    H2$obs[t, sy] <- vc[1] / sum(vc)
+    # Parametric bootstrapping from the model
+    para_boot <- bootMer(m, herit, nsim = nreps)
+    H2$lower[t, sy] <- quantile(para_boot$t, 0.025)
+    H2$upper[t, sy] <- quantile(para_boot$t, 0.975)
+  }
+}
+paste("Parametric bootstraps completed after", round((proc.time()-t0)[3] / 60, 2), "minutes.")
+
+expt <- split(ind_data, ind_data$site_year)
+H2$n <- sapply(expt, function(x) colSums(!is.na(x[, traits])))
+H2$n <- as.data.frame(H2$n)
+
+
+saveRDS(H2, file = "output/heritabilities.rds")
