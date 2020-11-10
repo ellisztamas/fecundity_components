6b75be1e71526f8b0baf8880105a0bbf7995685a
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2020-11-10 15:06:36 +0100

    Genetic correlations changed to Pearson. Moved manuscript file to its own folder for this submission Moved the supplementary figures to the same document as the main text as an appendix. Incorporated changes from google drive into the introduction.  Changes to be committed: 	modified:   R/014.genetic_correlations.R 	renamed:    manuscript/fecundity_components.Rmd -> manuscript/mol_ecol_2nd_submission/fecundity_components.Rmd 	renamed:    manuscript/supporting_information.Rmd -> manuscript/mol_ecol_2nd_submission/supporting_information.Rmd 	modified:   output/genetic_correlations.csv

--- R/014.genetic_correlations.R
+++ R/014.genetic_correlations.R
@@ -1,3 +1,5 @@
+set.seed(240)
+
 # RIL phenotypes
 ril_numb <- read.csv('data_derived/RIL_seeds_per_fruit.csv')
 ril_mass <- read.csv('data_derived/RIL_seed_mass.csv')
@@ -20,7 +22,8 @@ cortests <- list(
   cor.test(ril_tofu$it2010, ril_mass$it2010, method = 'p'),
   cor.test(ril_tofu$it2011, ril_mass$it2011, method = 'p'),
   cor.test(ril_tofu$sw2010, ril_mass$sw2010, method = 'p'),
-  cor.test(ril_tofu$sw2011, ril_mass$sw2011, method = 'p'))
+  cor.test(ril_tofu$sw2011, ril_mass$sw2011, method = 'p')
+  )
 
 cor_obs <- sapply(cortests, function(x) x$estimate)
 
@@ -50,14 +53,14 @@ for(r in 1:nreps){
   cor_bootstraps[r,] <- sapply(cortests_boot, function(x) x$estimate)
 }
 
-rhos <- data.frame(
+correlations <- data.frame(
   traits= rep(c("tofu_vs_surv", "frut_vs_numb", "tofu_vs_mass"), each=4),
   expt  = rep(colnames(ril_mass[-1]), 3),
-  n     = sapply(cortests, function(x) x$parameter) + 2,
+  df    = sapply(cortests, function(x) x$parameter),
   obs   = sapply(cortests, function(x) x$estimate),
   p     = sapply(cortests, function(x) x$p.value),
   lower = apply(cor_bootstraps, 2, quantile, 0.025),
   upper = apply(cor_bootstraps, 2, quantile, 0.975)
 )
 
-write.csv(rhos, 'output/genetic_correlations.csv', row.names = F)
+write.csv(correlations, 'output/genetic_correlations.csv', row.names = F)
